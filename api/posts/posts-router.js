641a5b439546fb17bd20617cd8d906c7082e55e7
HALİL ÜNAL <125642299+Halil-Unal@users.noreply.github.com>
2023-05-16 16:24:06 +0300

    first five done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,112 @@
 // posts için gerekli routerları buraya yazın
+const router = require('express').Router();
+
+const { json } = require('express');
+const {  find,
+    findById,
+    insert,
+    update,
+    remove,
+    findPostComments,
+    findCommentById,
+    insertComment, } = require("./posts-model");
+
+
+   router.get("/", async (req, res) => {
+        try {
+          const allUsers = await find();
+          res.json(allUsers);
+        } catch (error) {
+          res.status(500).json({ message: "Kullanıcı bilgileri alınamadı" });
+        }
+      });
+
+
+router.get("/:id",async (req,res)=>{
+try{
+    const user = await findById(req.params.id) ;
+    if(!user){
+        res.status(404)
+        .json({message:"veri bulunamadı"})
+    }
+    else{
+        res.json(user);
+    }
+}
+catch(error){
+    res.status(500),json({message:"bilgi alınamadı"})
+}
+})
+router.post("/",async(req,res)=>{
+    try{
+       const {title,contents,created_at,updated_at}=req.body;
+       if (!title || !contents || !created_at || !updated_at) {
+        res
+          .status(400)
+          .json({ message: "Lütfen kullanıcı için bilgileri sağlayınsağlayın" });
+      }
+      else {
+        const inserted = await insert({ title:title,contents:contents,created_at:created_at,updated_at:updated_at });
+        res.status(201).json(inserted);
+      }
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Veritabanına kaydedilirken bir hata oluştu" });
+    }
+})
+
+router.put("/:id",async (req,res)=>{
+    try{
+        const {title,contents,created_at,updated_at}=req.body;
+        if (!title || !contents || !created_at || !updated_at) {
+            res
+              .status(400)
+              .json({ message: "Lütfen kullanıcı için bilgileri sağlayınsağlayın" });
+          }
+          else  {
+            const user = await findById(req.params.id);
+            if(!user){
+                res
+                .status(400)
+                .json({message:"verilen id ye ait bileşen bulunamadı"})
+            }
+          
+          else {
+            const updateuser = await update(req.params.id,{ title:title,contents:contents,created_at:created_at,updated_at:updated_at })
+            res.json(updateuser);
+          }
+        }
+    }
+    catch{
+        res.status(500).json({ message: "Kullanıcı bilgisi alınamadı" });
+    }
+})
+
+router.delete("/:id",async(req,res)=>{
+try{
+
+    const user = await findById(req.params.id);
+    if(!user){
+res
+.status(400)
+.json({message:"verilen id ye ait bileşen bulunamadı"})
+    }
+    else{
+        const updatedata=await remove(req.params.id);
+        res.json(updatedata);
+    }
+
+
+}
+catch{ 
+    res.status(500).json({ message: "Kullanıcı bilgisi alınamadı" });
+  }
+
+})
+
+
+
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,3 +1,17 @@
 // server için gerekli olanları burada ayarlayın
 
 // posts router'ını buraya require edin ve bağlayın
+const express = require("express");
+const server = express();
+
+const datarouter = require("./posts/posts-router");
+server.use(express.json());
+
+server.use('/api/datas', datarouter);
+
+server.get('/', (req, res) => {
+    res.send('Server is up and running!...')
+});
+
+module.exports = server;
+
